aed898dfb25729808f223951fd94366e09089e14
mguimard <morgan.guimard@docdoku.com>
2018-08-10 16:48:26 +0200

    Prepare 2.5.0 release

--- loadSample.sh
+++ loadSample.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn clean install && mvn dependency:copy-dependencies && java -classpath target/docdoku-plm-sample-data.jar:target/dependency/docdoku-api-java-2.5-SNAPSHOT.jar:target/dependency/* com.docdoku.loaders.Main $*
+mvn clean install && mvn dependency:copy-dependencies && java -classpath target/docdoku-plm-sample-data.jar:target/dependency/docdoku-api-java-2.5.0.jar:target/dependency/* com.docdoku.loaders.Main $*
