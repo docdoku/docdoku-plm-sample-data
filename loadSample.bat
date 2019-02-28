16198a3d7443c92cb8b4c1039c9b0c429d34f428
Morgan Guimard <morgan.guimard@docdoku.com>
2019-02-28 09:02:11 +0100

    Prepare next release

--- loadSample.bat
+++ loadSample.bat
@@ -1,3 +1,3 @@
 mvn clean install;
 mvn dependency:copy-dependencies;
-java -classpath target\docdoku-plm-sample-data.jar;target\dependency\docdoku-api-java-2.5.2-SNAPSHOT.jar:target\dependency\* com.docdoku.loaders.Main
+java -classpath target\docdoku-plm-sample-data.jar;target\dependency\docdoku-api-java-2.5.2.jar:target\dependency\* com.docdoku.loaders.Main
--- loadSample.sh
+++ loadSample.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn clean install && mvn dependency:copy-dependencies && java -classpath target/docdoku-plm-sample-data.jar:target/dependency/docdoku-api-java-2.5.2-SNAPSHOT.jar:target/dependency/* com.docdoku.loaders.Main $*
+mvn clean install && mvn dependency:copy-dependencies && java -classpath target/docdoku-plm-sample-data.jar:target/dependency/docdoku-api-java-2.5.2.jar:target/dependency/* com.docdoku.loaders.Main $*
